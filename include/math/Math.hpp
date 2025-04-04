e285255cf920c3513d58acf746e6781ba9d491c1
Tem3dy <tem3dy@gmail.com>
2025-04-04 23:00:39 +0200

    feature: Add a basic math namespace

---
+++ include/math/Math.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+namespace Math
+{
+    template <typename T>
+    constexpr T Clamp(T value, T min, T max)
+    {
+        if (value < min)
+            return min;
+        if (value > max)
+            return max;
+        
+        return value;
+    }
+};
