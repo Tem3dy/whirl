4bb22d634765d45e4e3341657707e0ceee28582b
Tem3dy <tem3dy@gmail.com>
2025-04-11 01:24:28 +0200

    feature: Implement guards in DrawQuad

--- src/renderer/Renderer.cpp
+++ src/renderer/Renderer.cpp
@@ -50,4 +50,29 @@ void Renderer::DrawQuad(float x, float y, float w, float h, uint32_t color)
         std::cerr << "ERROR: Tried to draw with a closed renderer" << std::endl;
         return;
     }
+
+    if (x < 0 || y < 0)
+    {
+        // This needs a logger badly
+        std::cerr << "ERROR: Invalid quad coordinates ("
+                  << x << ", " << y << ")" << std::endl;
+        return;
+    }
+
+    if (w <= 0 || h <= 0)
+    {
+        // Same here
+        std::cerr << "ERROR: Invalid quad dimensions ("
+                  << w << ", " << h << ")" << std::endl;
+        return;
+    }
+
+    if (m_quadList.size() == m_quadList.capacity())
+    {
+        // TODO: Should be DEBUG instead of INFO, log more details later on
+        std::cout << "INFO: Reserving more memory for rendering quads" << std::endl;
+        m_quadList.reserve(m_quadList.capacity() * 2);
+    }
+
+    m_quadList.emplace_back(x, y, w, h, color);
 }
