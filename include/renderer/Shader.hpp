4ccf6ab53040466c86bfade04e32ebb62a85d51e
Tem3dy <tem3dy@gmail.com>
2025-04-10 04:42:26 +0200

    feature: Add basic uniform functionality to shaders

--- include/renderer/Shader.hpp
+++ include/renderer/Shader.hpp
@@ -12,6 +12,11 @@ public:
     bool Load(const std::string& path);
     void Use();
 
+    void SetBool(const std::string& name, bool value) const;
+    void SetInt(const std::string& name, int value) const;
+    void SetUInt(const std::string& name, unsigned int value) const;
+    void SetFloat(const std::string& name, float value) const;
+
 private:
     unsigned int m_program;
 };
--- src/renderer/Shader.cpp
+++ src/renderer/Shader.cpp
@@ -193,3 +193,51 @@ Shader::~Shader()
     if (m_program != 0)
         glDeleteProgram(m_program);
 }
+
+void Shader::SetBool(const std::string& name, bool value) const
+{
+    int location = glGetUniformLocation(m_program, name.c_str());
+    if (location == -1)
+    {
+        std::cout << "Tried uploading a uniform bool to an unknown variable: " << name << std::endl;
+        return;
+    }
+
+    glUniform1i(location, value);
+}
+
+void Shader::SetInt(const std::string& name, int value) const
+{
+    int location = glGetUniformLocation(m_program, name.c_str());
+    if (location == -1)
+    {
+        std::cout << "Tried uploading a uniform int to an unknown variable: " << name << std::endl;
+        return;
+    }
+
+    glUniform1i(location, value);
+}
+
+void Shader::SetUInt(const std::string& name, unsigned int value) const
+{
+    int location = glGetUniformLocation(m_program, name.c_str());
+    if (location == -1)
+    {
+        std::cout << "Tried uploading a uniform uint to an unknown variable: " << name << std::endl;
+        return;
+    }
+
+    glUniform1ui(location, value);
+}
+
+void Shader::SetFloat(const std::string& name, float value) const
+{
+    int location = glGetUniformLocation(m_program, name.c_str());
+    if (location == -1)
+    {
+        std::cout << "Tried uploading a uniform float to an unknown variable: " << name << std::endl;
+        return;
+    }
+
+    glUniform1f(location, value);
+}
