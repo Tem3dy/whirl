6e2de17aff4c10ccba98207113e23a1aaae143c0
Tem3dy <tem3dy@gmail.com>
2025-04-11 06:02:01 +0200

    feature: Add new functionality to Math namespace

--- include/math/Math.hpp
+++ include/math/Math.hpp
@@ -7,13 +7,25 @@
 namespace Math
 {
     template <typename T>
-    constexpr T Clamp(T value, T min, T max)
+    inline constexpr T Clamp(T value, T min, T max)
     {
         if (value < min)
             return min;
         if (value > max)
             return max;
-        
+
         return value;
+    }
+
+    template <typename T>
+    inline constexpr T Min(T x, T y)
+    {
+        return x < y ? x : y;
+    }
+
+    template <typename T>
+    inline constexpr T Max(T x, T y)
+    {
+        return x > y ? x : y;
     }
 };
