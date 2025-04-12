269c27c5211e5aa8404a21c2721ab42d87631eaf
Tem3dy <tem3dy@gmail.com>
2025-04-12 03:04:44 +0200

    refactor: Remove test rendering in GuiApplication

--- src/GuiApplication.cpp
+++ src/GuiApplication.cpp
@@ -10,8 +10,11 @@ GuiApplication::GuiApplication(const VideoMode& mode)
 
 int GuiApplication::Launch()
 {
-    glfwSetErrorCallback([](int error, const char* description)
-                         { std::cerr << "GLFW Error: " << error << ": " << description << std::endl; });
+    // clang-format off
+    glfwSetErrorCallback([](int error, const char* description) {
+        std::cerr << "GLFW Error: " << error << ": " << description << std::endl;
+    });
+    // clang-format on
 
     if (!glfwInit())
     {
@@ -52,8 +55,8 @@ int GuiApplication::Launch()
 
     // Set up viewport
     glViewport(0, 0, m_mode.width, m_mode.height);
-    glfwSetFramebufferSizeCallback(m_window, [](GLFWwindow* window, int width, int height)
-                                   {
+    // clang-format off
+    glfwSetFramebufferSizeCallback(m_window, [](GLFWwindow* window, int width, int height) {
         glViewport(0, 0, width, height);
 
         // TODO: Figure this out better at some point
@@ -63,7 +66,9 @@ int GuiApplication::Launch()
             app->m_mode.width = width;
             app->m_mode.height = height;
             app->m_renderer.Adjust(width, height);   
-        } });
+        }
+    });
+    // clang-format on
 
     m_renderer.Open();
     m_renderer.Adjust(m_mode.width, m_mode.height);
@@ -71,20 +76,9 @@ int GuiApplication::Launch()
     {
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+
         // App rendering and logic
 
-        m_renderer.DrawQuad(0, 0, 50, 50, Color::Red());
-        m_renderer.DrawQuad(1230, 0, 50, 50, Color::Green());
-        m_renderer.DrawQuad(0, 670, 50, 50, Color::Blue());
-        m_renderer.DrawQuad(1230, 670, 50, 50, Color::Yellow());
-        m_renderer.DrawQuad(615, 335, 50, 50, Color::White());
-
-        m_renderer.DrawVLine(100, 50, 200, 10, Color::Red());
-        m_renderer.DrawHLine(200, 100, 300, 15, Color::Green());
-        m_renderer.DrawVLine(400, 150, 100, 5, Color::Blue());
-        m_renderer.DrawHLine(50, 200, 500, 20, Color::Yellow());
-
-        m_renderer.Flush();
         glfwSwapBuffers(m_window);
         glfwPollEvents();
     }
