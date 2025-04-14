8fef40ee2c4c3fba7dd9a4effd8654e0776eba4a
Tem3dy <tem3dy@gmail.com>
2025-04-15 00:09:34 +0200

    refactor: Change static functions to inline in Color.hpp

--- include/renderer/Color.hpp
+++ include/renderer/Color.hpp
@@ -5,47 +5,47 @@
 // clang-format off
 namespace Color
 {
-    static constexpr uint32_t New(uint8_t r, uint8_t g, uint8_t b, uint8_t a = 0xff)
+    inline constexpr uint32_t New(uint8_t r, uint8_t g, uint8_t b, uint8_t a = 0xff)
     {
         return (r << 24) | (g << 16) | (b << 8) | a;
     }
     
-    static constexpr uint32_t White()
+    inline constexpr uint32_t White()
     {
         return New(0xff, 0xff, 0xff);
     }
 
-    static constexpr uint32_t Black()
+    inline constexpr uint32_t Black()
     {
         return New(0x00, 0x00, 0x00);
     }
 
-    static constexpr uint32_t Red()
+    inline constexpr uint32_t Red()
     {
         return New(0xff, 0x00, 0x00);
     }
 
-    static constexpr uint32_t Green()
+    inline constexpr uint32_t Green()
     {
         return New(0x00, 0xff, 0x00);
     }
 
-    static constexpr uint32_t Blue()
+    inline constexpr uint32_t Blue()
     {
         return New(0x00, 0x00, 0xff);
     }
 
-    static constexpr uint32_t Yellow()
+    inline constexpr uint32_t Yellow()
     {
         return New(0xff, 0xff, 0x00);
     }
 
-    static constexpr uint32_t Cyan()
+    inline constexpr uint32_t Cyan()
     {
         return New(0x00, 0xff, 0xff);
     }
 
-    static constexpr uint32_t Magenta()
+    inline constexpr uint32_t Magenta()
     {
         return New(0xff, 0x00, 0xff);
     }
