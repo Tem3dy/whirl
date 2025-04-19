78e6f8b8a37c8b9238b91c16e64ee4dbf37102f1
Tem3dy <tem3dy@gmail.com>
2025-04-19 20:15:09 +0200

    note: Add comment separations in GuiRenderer

--- include/renderer/GuiRenderer.hpp
+++ include/renderer/GuiRenderer.hpp
@@ -17,12 +17,15 @@ public:
     ~GuiRenderer();
     
 public:
+    // Quads/lines
     void DrawQuad(float x, float y, float w, float h, uint32_t color = Color::White());
     void DrawVLine(float x, float y, float length, float thickness, uint32_t color = Color::White());
     void DrawHLine(float x, float y, float length, float thickness, uint32_t color = Color::White());
 
+    // Rounded quads/lines
     void DrawRoundedQuad(float x, float y, float w, float h, float radius, uint32_t color = Color::White()); 
 
+    // Circles
     void DrawCircle(float x, float y, float radius, uint32_t color = Color::White());
 
     void Submit();
